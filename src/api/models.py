796f8281be977539dd31ea12d2a2fab8334ce0da
EL225 <ericleonmora99@gmail.com>
2022-07-05 03:01:04 +0000

    paso1

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,20 @@
-from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from eralchemy import render_er
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(Base):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_admin = db.Column(db.Boolean(False), unique=False, nullable=False)
 
-    def __repr__(self):
-        return f'<User {self.email}>'
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Perfil(Base):
+    __tablename__="perfil"
+    id_Usuario
