181d93c8ba0aed641ebf950db5fdbec5080852ac
alonsogomezo <luisalonsogo95@gmail.com>
2022-07-06 22:24:57 +0000

    login

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -26,7 +26,7 @@ const Layout = () => {
             <Route element={<Home />} path="/" />
             <Route element={<Demo />} path="/demo" />
             <Route element={<Single />} path="/single/:theid" />
-            <Route element={<Login />} path="/login/:caca/:otracaca" />
+            <Route element={<Login />} path="/login" />
             <Route element={<h1>Not found!</h1>} />
           </Routes>
           <Footer />
