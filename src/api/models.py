5f8b732902547dfce69884a9081a142f6f221710
EL225 <ericleonmora99@gmail.com>
2022-07-07 01:30:09 +0000

    database

--- src/api/models.py
+++ src/api/models.py
@@ -1,73 +1,58 @@
-import os
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
-from eralchemy import render_er
-
+from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
-class User(Base):
-    __tablename__ = "user"
-    id = Column(Integer, primary_key=True)
-    email = Column(String(120), unique=True, nullable=False)
-    password = Column(String(80), unique=False, nullable=False)
-    is_admin = Column(Boolean(False), unique=False, nullable=False)
+class User(db.Model):
+    id = db.Column(db.Interger, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_admin = db.Column(db.Boolean(False), unique=False, nullable=False)
 
-class Perfil(Base):
-    __tablename__="perfil"
-    id_Usuario = Column(Integer, ForeignKey("user.id"))
-    foto_perfil = Column(String(2000))
-    nombre = Column(String(10))
-    apellido = Column(string(10))
-    direccion = Column(String(50))
-    telefono = Column(Integer)
+class Perfil(db.Model):
+    id = db.Column(db.Interger, primary_key=True)
+    id_Usuario = db.Column(db.Interger, ForeignKey("User.id"))
+    foto_perfil = db.Column(db.String(2000))
+    nombre = db.Column(db.String(50))
+    apellido = db.Column(String(50))
+    direccion = db.Column(db.String(500))
+    telefono = db.Column(db.String(80))
 
-class Producto(Base):
-    __tablename__="producto"
-    id_producto = Column(Integer, primary_key=True)
-    id_usuario = Column(Interger, ForeignKey("user.id"))
-    categoria = Column(Integer, ForeignKey("categorias.id_categoria"))
-    SKU = Column(String)
-    Nomnbre = Column(String)
-    Precio = Column(Float)
-    Descripcion = Column(String)
+class Producto(db.Model):
+    id = db.Column(db.Interger, primary_key=True)
+    id_usuario = db.Column(db.Interger, ForeignKey("User.id"))
+    categoria = db.Column(db.Interger, ForeignKey("Categorias.id"))
+    Nombre = db.Column(db.String(20))
+    Precio = db.Column(db.Float)
+    Descripcion = db.Column(db.String(700))
 
-class categoria(Base):
-    __tablename__="categorias"
-    id_categoria = Column(Integer)
-    Nombre = Column(String)
+class Categorias(db.Model):
+    id = db.Column(db.Interger)
+    Nombre = db.Column(db.String(20))
 
-class Orden_Items(Base):
-    __tablename__="OI"
-    id_Producto = Column(Integer, ForeignKey("producto.id_producto"))
-    Precio = Column(Float)
-    Cantidad = Column(Integer)
-    Subtotal = Column(Float)
+class Orden_Items(db.Model):
+    id = db.Column(db.Interger, primary_key=True)
+    id_Producto = db.Column(db.Interger, ForeignKey("Producto.id"))
+    Precio = db.Column(db.Float)
+    Cantidad = db.Column(db.Interger)
+    Subtotal = db.Column(db.Float)
 
-class Order_Status(Base):
-    __tablename__="OS"
-    id_OS = Column(Integer)
-    estado = Column(String)
+class Order_Status(db.Model):
+    id = db.Column(db.Interger)
+    estado = db.Column(db.String(50))
 
-class Carrito(Base):
-    __tablename__="Carrito"
-    id_Carrito = Column(Integer)
-    id_Producto = Column(Integer, ForeignKey("producto.id_producto"))
-    id_Usuario = Column(Integer, ForeignKey("user.id"))
+class Carrito(db.Model):
+    id = db.Column(db.Interger)
+    id_Producto = db.Column(db.Interger, ForeignKey("Producto.id"))
+    id_Usuario = db.Column(db.Interger, ForeignKey("User.id"))
 
-class Orden(Base):
-    __tablename__="Orden"
-    id_orden = Column(Integer)
-    Total = Column(Float)
-    Total_Cantidad = Column(Integer)
-    id_Usuario = Column(Integer, ForeignKey("user.id"))
-    id_OS = Column(Integer, ForeignKey("OS.id_OS"))
+class Orden(db.Model):
+    id_orden = db.Column(db.Interger)
+    Total = db.Column(db.Float)
+    Total_Cantidad = db.Column(db.Interger)
+    id_Usuario = db.Column(db.Interger, ForeignKey("User.id"))
+    id_OS = db.Column(db.Interger, ForeignKey("Order_Status.id"))
 
-class Pago(Base):
-    __tablename__="Pago"
-    id_Pago = Column(Integer)
-    id_Usuario = Column(Integer, ForeignKey("user.id"))
-    id_OS = Column(Integer, ForeignKey("OS.id_OS"))
-    monto_Total = Column(Float)
+class Pago(db.Model):
+    id = db.Column(db.Interger)
+    id_Usuario = db.Column(db.Interger, ForeignKey("User.id"))
+    id_OS = db.Column(db.Interger, ForeignKey("Order_Status.id"))
+    monto_Total = db.Column(db.Float)
