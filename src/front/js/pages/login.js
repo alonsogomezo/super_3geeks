bfec2c1815bfae924e8e3cb68692bf3fca18ef3a
alonsogomezo <luisalonsogo95@gmail.com>
2022-07-09 03:00:09 +0000

    avance

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  const handleSubmit = (e, x) => {
-    e.preventDefault();
-    console.log(e);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmit = () => {
+    alert(email);
   };
   return (
     <div className="container">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={onSubmit}>
         <div className="form-group row p-3 justify-content-center">
           <div class="col-10 col-sm-5">
             <label for="inputEmail3" className="col-sm-2 col-form-label">
@@ -15,6 +16,8 @@ const Login = () => {
             </label>
             <input
               required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               type="email"
               className="form-control"
               id="inputEmail3"
