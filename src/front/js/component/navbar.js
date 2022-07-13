90937cfa151b6d87c5c5c72ce56a317320a12551
alonsogomezo <luisalonsogo95@gmail.com>
2022-07-13 01:49:39 +0000

    avanceee

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,26 @@
-import React from "react";
 import { Link } from "react-router-dom";
-
+import React, { useContext, useState, useEffect } from "react";
+import { Context } from "../store/appContext";
+import Logo from "../../img/logo-3geeks.png";
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  const { store } = useContext(Context);
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <Link to="/" className="text-decoration-none">
+            <img src={Logo} width="40" />
+          <span className="navbar-brand mb-0 h1 pl-5">Super 3 geeks</span>
+        </Link>
+        <div className="ml-auto">
+          {store?.user?.accessToken ? (
+            "Bienvenido"
+          ) : (
+            <Link to="/login">
+              <button className="btn btn-danger">Login</button>
+            </Link>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 };
