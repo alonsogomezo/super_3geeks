d536e97cf8525a5674c7437555033ab0e6ed4a2a
Steelver <1steelver@gmail.com>
2022-07-20 20:14:55 +0000

    amorVisual2ELREGRESO

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
+import Logo from "../../img/logo-3geeks.png";
 
 export const Footer = () => (
   
-    <footer className="footer bg-ligth text-dark mt-auto py-3 text-center">
+    <footer className="footer bg-dark text-white mt-auto py-3 text-center">
+
+
       <p>
       Tried by <i className="fa fa-heart text-danger" /> 
          TEAM-3Geeks
         <i className="fa fa-heart text-danger" /> 
         
+      </p>
+      <p>
+        This boilerplate comes with lots of documentation:{" "}
+        <a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
+          Read documentation
+        </a>
       </p>
       
     </footer>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -9,72 +9,117 @@ export const Home = () => {
 
   return (
     <div className="text-center mt-0">
+      <nav className="navbar navbar-warning bg-danger text-center">
+        <form className="form-inline">
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <button className="input-group-text" type="submit">
+                @
+              </button>
+            </div>
+            <input
+              type="search"
+              className="form-control"
+              placeholder="Que estas  buscando?"
+              aria-label="search"
+            />
+          </div>
+        </form>
+      </nav>
 
-		
-     
-        <img height={200} className="container-fluid "
-         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzKRPlFqXEPtFeIkaEexRkj77bLIpN9ZMmIQ&usqp=CAU"/>
-          {/*
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTG_maK1oK7T1pF_dTPNeNeoYpaWTIEnbPJg&usqp=CAU"
-        className=""
-      />
-  */}
-
- 
-
-      <div className="jumbotron mt-3">
-        
-        <h1 className="text-warning">Super 3 Geeks<img src={Logo} width={60} /></h1>
-        <p className="text-danger">Resize this responsive page to see the effect!</p>
-        
+      <div
+        id="carouselExampleSlidesOnly"
+        class="carousel slide"
+        data-ride="carousel"
+      >
+        <div class="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              height={200}
+              classaName="d-block w-100"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTG_maK1oK7T1pF_dTPNeNeoYpaWTIEnbPJg&usqp=CAU"
+              alt="First slide"
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>..1.</h5>
+              <p>...</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img
+              height={200}
+              className="d-block w-100"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzKRPlFqXEPtFeIkaEexRkj77bLIpN9ZMmIQ&usqp=CAU"
+              alt="Second slide"
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>.2..</h5>
+              <p>...</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img
+              height={200}
+              className="d-block w-100"
+              src="..."
+              alt="Third slide"
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>3...</h5>
+              <p>...</p>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="container">
         <div className="row">
           <div className="col-sm-4">
-            <h3>Categoria 1</h3>
-            <p>Lorem ipsum dolor..</p>
-			<img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcp1DCq1CA4svPvY40YJmOn_POXn3Za3ut2g&usqp=CAU"
-        className=""
-      />
+            <h3>nombre</h3>
+            <p>Descripcion del producto</p>
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcp1DCq1CA4svPvY40YJmOn_POXn3Za3ut2g&usqp=CAU"
+              className=""
+            />
+
+            <div className="btn-group btn-group-lg" role="group" aria-label="...">
+              -
+            </div>
+            <div className="btn-group" role="group" aria-label="...">
+              0
+            </div>
+            <div className="btn-group btn-group-sm" role="group" aria-label="...">
+              +
+            </div>
+
+            <a href="#" class="btn btn-danger rounded-circle">
+              añadir al carrito
+            </a>
           </div>
-          <div className="col-sm-4" width={200} height={300} >
-            <h3>Categoria 2</h3>
-            <p>CaracteristicasLorem ipsum dolor..</p>
-			<img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgaGIWPc_K5FvUFlLIhWWbbe5UsBhnQrvuUg&usqp=CAU"
-        className=""
-      />
+          <div className="col-sm-4" width={200} height={300}>
+            <h3>nombre</h3>
+            <p>Descripcion delmproducto</p>
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgaGIWPc_K5FvUFlLIhWWbbe5UsBhnQrvuUg&usqp=CAU"
+              className=""
+            />
+            <a href="#" class="btn btn-danger rounded-circle">
+              añadir al carrito
+            </a>
           </div>
           <div className="col-sm-4">
-            <h3>Categoria 3</h3>
-            <p>Lorem ipsum dolor..</p>
-			<img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBgxV2SX3o1WwNoeaaUcuIL6CVSLP9-JHCCw&usqp=CAU"
-        className=""
-      />
+            <h3>nombre</h3>
+            <p>descripcion del producto</p>
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBgxV2SX3o1WwNoeaaUcuIL6CVSLP9-JHCCw&usqp=CAU"
+              className=""
+            />
+            <a href="#" className="btn btn-danger rounded-circle">
+              añadir al carrito
+            </a>
           </div>
         </div>
       </div>
-
-   
-
-      <img src="paris.jpg" className="float-right" />
-	  <p>
-        This boilerplate comes with lots of documentation:{" "}
-        <a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-          Read documentation
-        </a>
-      </p>
-      <div className="alert alert-info bg-dark">
-        {store.message ||
-          "Loading message from the backend (make sure your python backend is running)..."}
-      </div>
-	
-
-    
     </div>
   );
 };
