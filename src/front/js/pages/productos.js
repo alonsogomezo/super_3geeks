58d83f4992407d8391020073c0cd57479ddc78ad
Steelver <1steelver@gmail.com>
2022-07-22 16:32:56 +0000

    1

--- src/front/js/pages/productos.js
+++ src/front/js/pages/productos.js
@@ -35,9 +35,31 @@ export const Productos = () => {
     <div className="text-center mt-0">
       <form onSubmit={onSubmit}>
         <div className="row">
-          <div class="alert alert-danger" role="alert">
-            This is a primary alert—check it out!
+        <nav className="navbar navbar-warning bg-danger ">
+        <form className="form-inline">
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <button className="input-group-text" type="submit">
+                @
+              </button>
+            </div>
+            <input
+              type="search"
+              className="form-control"
+              placeholder="Que estas  buscando?"
+              aria-label="search"
+            />
           </div>
+        </form>
+
+        <button
+          type="button"
+          className="btn btn-secondary rounded-circle fa-thin fa-cart-shopping"
+        >
+          carrito button
+        </button>
+      </nav>
+          <hr/>
           <div className="col-6">                      
                   <img
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgaGIWPc_K5FvUFlLIhWWbbe5UsBhnQrvuUg&usqp=CAU"
@@ -49,7 +71,7 @@ export const Productos = () => {
           <div className="col-6 mt-5">
             <div className="jumbotron mt-3">
               <h1 className="text-dark">
-                Super 3 Geeks
+                Nombre del Producto
                 <img src={Logo} width={60} />
               </h1>
             </div>
@@ -59,14 +81,16 @@ export const Productos = () => {
               que corresponda y luego ingrese los datos requeridos en los campos
             </p>
           
-            <h5 className="card-title">Envio de Datos</h5>
+            <h5 className="card-title">Descripcion</h5>
 
             <p className="card-text">
-              Recuerda que si alguno de los campos carece de informacion no sera
-              posible realizar dicha accion
+            Descripcion del producto
             </p>
 
+          
+
           </div>
+          
         </div>
       </form>
     </div>
